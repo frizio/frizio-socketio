b497d4c67e604e225a294d6fefaab5b7b724306e
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-03-18 14:42:46 +0100

    path.join( __dirname, "/public")

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
+import path from "path";
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join( __dirname, "/public")));
 
 export default app;
