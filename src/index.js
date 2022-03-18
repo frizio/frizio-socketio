f2bfbbae4a693c380bcd627fcccc4dbc632ff043
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-03-18 15:00:40 +0100

    Sockets config in separate file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import http from "http";
 import app from "./app";
 import { PORT } from './config'
 import { Server as WebSocketServer } from "socket.io";
+import sockets from "./sockets";
 
 const main = () => {
 
@@ -10,21 +11,7 @@ const main = () => {
   console.log(`Server running on port ${PORT}`)
 
   const io = new WebSocketServer(httpServer);
-  io.on('connection', (socket) => {
-    console.log(`New socket connection. id: `, socket.id);
-    socket.emit('ping');
-
-    socket.on('pong', () => {
-      console.log('Ricevuto pong');
-    });
-
-    socket.on('client:newnote', (data) => {
-      console.log('Ricevuti dati');
-      console.log(data);
-    });
-
-  })
-  // To verify: http://localhost:3001/socket.io/socket.io.min.js
+  sockets(io);
 
 };
 
---
+++ src/sockets.js
@@ -0,0 +1,18 @@
+export default (io) => {
+  io.on("connection", (socket) => {
+    console.log(`New socket connection. id: `, socket.id);
+    console.log(socket.handshake)
+    socket.emit("ping");
+
+    socket.on("pong", () => {
+      console.log("Ricevuto pong");
+    });
+
+    socket.on("client:newnote", (data) => {
+      console.log("Ricevuti dati");
+      console.log(data);
+    });
+    
+  });
+  // To verify: http://localhost:3001/socket.io/socket.io.min.js
+};
